d42216ac126e5455a31786a57a3dab8db65559f5
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2024-04-08 10:57:26 -0300

    :tada: init

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateUser extends Command
+{
+    protected $signature = 'user:create {name} {email} {password}';
+    protected $description = 'Create a new user';
+
+    public function handle()
+    {
+        $user = User::create([
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => bcrypt($this->argument('password')),
+        ]);
+
+        $this->info('User created successfully!');
+    }
+}
